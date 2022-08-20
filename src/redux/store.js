10b84e70f43cf72ff7168c94993954ea8b72cc5c
jho951 <jho951@naver.com>
2022-08-20 10:17:35 +0900

    리덕스툴킷 폴더 생성

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import dayDataSlice from "./dayDataSlice";
+import userSlice from "./userSlice";
+
+export const store = configureStore({
+  reducer: {
+  },
+});
