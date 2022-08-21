d0272cf7e802526bc1aa2238c9654970f9b05593
hyexjun <hyebyjun@gmail.com>
2022-08-21 22:35:05 +0900

    chore : react 버전 다운그레이드 18 > 17

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-  // <Provider store={store}>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+// react 17
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+render(
+  <StrictMode>
     <App />
-  // </Provider>
-  // </React.StrictMode>
+  </StrictMode>,
+  rootElement
 );
