e55ca4bc1b06d2eabd1017c12192362249aa7f0d
hyexjun <hyebyjun@gmail.com>
2022-08-22 00:00:03 +0900

    feat : 라우터 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Main from './pages/Main';
 import Detail from './pages/Detail';
-import MyPage from './pages/MyPage';
+import MyVelog from './pages/MyVelog';
+import Lists from './pages/Lists';
 import Write from './pages/Write';
+import Setting from './pages/Setting'
 import NotFound from './pages/NotFound';
-import Header from './components/Header';
 
 function App() {
   return (
@@ -12,9 +14,12 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/mypage" element={<MyPage />} />
         <Route path="/write" element={<Write />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/myvelog" element={<MyVelog />} />
+        <Route path="/lists" element={<Lists />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/setting" element={<Setting />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
