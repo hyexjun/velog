1d0b52abd6d9fdee0424be4f3976f4533389ba8d
jho951 <jho951@naver.com>
2022-08-23 10:44:02 +0900

    로그인 여부에 따라 다르게 헤더 화면 구성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { Button, Container, Nav, Navbar, Modal } from 'react-bootstrap';
 import LoginModal from './LoginModal';
+import Cookies from 'universal-cookie';
+import { getCookie } from '../shared/Cookie';
 
 const Header2 = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  
+  const view = getCookie('accessToken')
+  console.log(view);
+  const [isLoggedin,setisLoggedin]=useState(false);
+  useEffect(()=>{if(view!==undefined){
+    setisLoggedin(true);
+  }else{
+    setisLoggedin(false);
+  }},[view]);
 
   const navigate = useNavigate();
 
@@ -23,23 +34,23 @@ const Header2 = () => {
           <Nav className="header-menu">
             <i className="fa-solid fa-sun" />
             <i className="fa-solid fa-magnifying-glass" />
-            <Button variant="outline-dark" onClick={() => navigate('/write')}>
+            {isLoggedin? (<Button variant="outline-dark" onClick={() => navigate('/write')}>
               새 글 작성
-            </Button>
-            <li className="nav-item dropdown">
+            </Button>):(<></>)}
+            {isLoggedin ?( <li className="nav-item dropdown">
               <div
                 className="nav-link dropdown-toggle"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                <img
+               <img
                   alt="프로필 사진"
                   style={{ width: '45px', borderRadius: '100%' }}
                   src="https://velog.velcdn.com/images/hyexjun/profile/108c8f1a-b604-4881-9906-00270be78272/image.jpg"
                 />
-              </div>
-              <ul className="dropdown-menu">
+             </div>
+            <ul className="dropdown-menu">
                 <li
                   className="dropdown-item"
                   onClick={() => navigate('/myvelog')}
@@ -60,11 +71,11 @@ const Header2 = () => {
                 </li>
                 <li className="dropdown-item">로그아웃</li>
               </ul>
-            </li>
+            </li>):(<></>)}
 
-            <Button variant="dark" onClick={handleShow}>
+            {isLoggedin ?(<></>):(<Button variant="dark" onClick={handleShow}>
               로그인
-            </Button>
+            </Button>)}
           </Nav>
           {/* </Navbar.Collapse> */}
         </Container>
