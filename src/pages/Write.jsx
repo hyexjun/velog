d21c5446c8d4b5c114e989c94de93c3eeeaec430
hyexjun <hyebyjun@gmail.com>
2022-08-23 18:06:41 +0900

    .

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,24 +1,27 @@
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
-import { useState,useRef } from 'react';
+import { useState, useRef } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import apis from '../api';
 
-
 const Write = () => {
-  const navigate=useNavigate();
-  const titleRef=useRef();
-  const contentsRef=useRef();
-  const tagsRef=useRef();
+  const navigate = useNavigate();
+  const titleRef = useRef();
+  const contentsRef = useRef();
+  const tagsRef = useRef();
   // const title=titleRef.current.value;
   // const contents=contentsRef.current.value;
   // const tags=tagsRef.current.value;
-  const [tagList,setTagList] = useState([]);
-  const SubmitHandler=()=>{
-    const tags=tagsRef.current.value;
+
+  const [tagList, setTagList] = useState([]);
+
+  const SubmitHandler = () => {
+    const tags = tagsRef.current.value;
     console.log(tags);
-  }
+  };
+
+  // 출간하기 실행 시 api 호출
   const WriteHandler = () => {
     const title = titleRef.current.value;
     const contents = contentsRef.current.value;
@@ -32,31 +35,43 @@ const Write = () => {
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   };
-  //엔터키 입력확인하는 함수
-  const EnterKeyInput = () =>{
-      if(window.event.keyCode == 13 && tagsRef.current.value !== ''){
-        setTagList([...tagList,tagsRef.current.value])
-        tagsRef.current.value = '';
-      }
-  }
-  const Exit=()=>{
+
+  //엔터키 입력 확인하는 함수
+  const EnterKeyInput = () => {
+    if (window.event.keyCode === 13 && tagsRef.current.value !== '') {
+      setTagList([...tagList, tagsRef.current.value]);
+      tagsRef.current.value = '';
+    }
+  };
+  const Exit = () => {
     navigate('/');
-  }
+  };
   return (
     <Layout>
       <StTitle>
-        <StInputTitle placeholder='제목을 적어주세요' ref={titleRef} />
+        <StInputTitle placeholder="제목을 적어주세요" ref={titleRef} />
         <div>
-        <input type='text' onKeyUp={EnterKeyInput} ref={tagsRef} onChange={SubmitHandler}/>
-        <div style={{display:"flex", flexDirection:'column', gap: '3px'}}>
-        {tagList.map((tag) =>{
-          return (
-            <div style={{background:"orange", border:'none', borderRadius:"2px"}}>{tag}</div>)
-        })}
+          <input
+            type="text"
+            onKeyUp={EnterKeyInput}
+            ref={tagsRef}
+            onChange={SubmitHandler}
+          />
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '3px' }}>
+            {tagList.map((tag) => (
+              <div
+                style={{
+                  background: 'orange',
+                  border: 'none',
+                  borderRadius: '2px',
+                }}
+              >
+                {tag}
+              </div>
+            ))}
+          </div>
         </div>
-        </div>
-        <StCopyTitle />
-      </StTitle> 
+      </StTitle>
       {/* <Editor
         initialValue="hello react editor world!"
         previewStyle="vertical"
@@ -64,9 +79,23 @@ const Write = () => {
         initialEditType="markdown"
         useCommandShortcut={true}
       /> */}
-      <StTextarea name="" id="" cols="30" rows="10" placeholder='내용을 적어주세요' ref={contentsRef}></StTextarea>
+      <StTextarea
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        placeholder="내용을 적어주세요"
+        ref={contentsRef}
+      ></StTextarea>
       <StSubmitBox>
-        <i className="fa-solid fa-arrow-left" onClick={Exit} style={{cursor:'pointer'}}> 나가기</i>
+        <i
+          className="fa-solid fa-arrow-left"
+          onClick={Exit}
+          style={{ cursor: 'pointer' }}
+        >
+          {' '}
+          나가기
+        </i>
         <StPostButton>출간하기</StPostButton>
       </StSubmitBox>
     </Layout>
@@ -79,10 +108,6 @@ const StTitle = styled.div`
   display: flex;
 `;
 const StInputTitle = styled.input`
-  width: 50%;
-  height: 10vh;
-`;
-const StCopyTitle = styled.input`
   width: 50%;
   height: 10vh;
 `;
