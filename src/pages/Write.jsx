4e7abf1978b1679f6a988173e751edfb3f7c80d6
hyexjun <hyebyjun@gmail.com>
2022-08-23 20:33:16 +0900

    css : 게시글 작성페이지 뷰 수정

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -16,10 +16,10 @@ const Write = () => {
 
   const [tagList, setTagList] = useState([]);
 
-  const SubmitHandler = () => {
-    const tags = tagsRef.current.value;
-    console.log(tags);
-  };
+  // const SubmitHandler = () => {
+  //   const tags = tagsRef.current.value;
+  //   console.log(tags);
+  // };
 
   // 출간하기 실행 시 api 호출
   const WriteHandler = () => {
@@ -31,8 +31,7 @@ const Write = () => {
         title: title,
         contents: contents,
         tags: tags,
-      }
-      )
+      })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   };
@@ -50,29 +49,39 @@ const Write = () => {
   return (
     <Layout>
       <StTitleInput placeholder="제목을 입력하세요" ref={titleRef} />
-      <div>
-        <StTagInput
-          type="text"
-          placeholder="태그를 입력하세요"
-          onKeyUp={EnterKeyInput}
-          ref={tagsRef}
-          onChange={SubmitHandler}
-        />
-        <div style={{ display: 'flex', gap: '3px' }}>
-          {tagList.map((tag, idx) => (
-            <div
-              key={idx}
-              style={{
-                background: 'orange',
-                border: 'none',
-                borderRadius: '2px',
-              }}
-            >
-              {tag}
-            </div>
-          ))}
-        </div>
+      <StTagInput
+        type="text"
+        placeholder="태그를 입력하세요"
+        onKeyUp={EnterKeyInput}
+        ref={tagsRef}
+        // onChange={SubmitHandler}
+      />
+      <div
+        style={{
+          display: 'flex',
+          gap: '5px',
+          // border: '1px solid blue',
+          height: '5vh',
+          alignItems: 'center',
+          color: '#12b886',
+        }}
+      >
+        {tagList.map((tag, idx) => (
+          <div
+            key={idx}
+            style={{
+              background: '#e0e0e0',
+              border: 'none',
+              borderRadius: '20px',
+              height: '35px',
+              padding: '5px 15px',
+            }}
+          >
+            {tag}
+          </div>
+        ))}
       </div>
+
       {/* <Editor
         initialValue="hello react editor world!"
         previewStyle="vertical"
@@ -107,35 +116,39 @@ const Layout = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border: 1px solid red
+  /* border: 1px solid red; */
+  gap: 0.5rem;
 `;
 const StTitleInput = styled.input`
-  width: 80%;
+  width: 100%;
   height: 7vh;
+  border: none;
 `;
 const StTagInput = styled.input`
-  width: 80%;
+  width: 100%;
   height: 5vh;
+  border: none;
 `;
 const StTextarea = styled.textarea`
-  width: 80%;
+  width: 100%;
   height: 50vh;
+  border: none;
 `;
 const StSubmitBox = styled.div`
-  width: 80%;
+  width: 100%;
   height: 50px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   font-size: 18px;
-  border: 1px solid red;
+  /* border: 1px solid red; */
 `;
 const StPostButton = styled.button`
   background-color: #12b886;
   color: white;
   border: none;
   border-radius: 5px;
-  width: 100px;
+  width: 110px;
   height: 80%;
 `;
 export default Write;
