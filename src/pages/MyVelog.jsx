a53d0e66fa790c6d291532fb3c650e0d3e50c584
hyexjun <hyebyjun@gmail.com>
2022-08-24 18:41:40 +0900

    feat : 특정게시글 상세페이지 뷰 완성도 높이기 #4

--- src/pages/MyVelog.jsx
+++ src/pages/MyVelog.jsx
@@ -22,7 +22,7 @@ const MyVelog = () => {
       <div
         style={{
           display: 'flex',
-          maxWidth: '700px',
+          maxWidth: '800px',
           border: '1px solid green',
           margin: '10px auto',
         }}
@@ -45,24 +45,36 @@ const MyVelog = () => {
       </div>
       <hr style={{ maxWidth: '700px', margin: '20px auto' }} />
       {/* 내 작성글들 */}
-      <div style={{ border: '1px solid pink', display: 'flex', margin:'0 10%' }}>
-        <div style={{ border: '1px solid orange', width:'200px' }}>
+      <div
+        style={{
+          border: '5px solid pink',
+          display: 'flex',
+          justifyContent: 'space-between',
+          maxWidth: '1000px',
+          margin: '0 auto'
+        }}
+      >
+        {/* 태그 카테고리 섹션 */}
+        <div
+          style={{
+            border: '1px solid orange',
+            width: '220px',
+            minWidth: '180px',
+          }}
+        >
           <div>태그 목록</div>
+          <hr />
           <div>전체보기</div>
-          <div>태그 맵 뿌려주기</div>
+          <div>모든 태그 맵 뿌리기 (3)</div>
         </div>
-        <div style={{ border: '1px solid pink' }}>
-          {/* 여기는 맵으로 카드 뿌려주기 */}
-          <div>
-            <div>글 제목</div>
-            <div>글 본문 글 본문 글 본문</div>
-            <div>태그 목록</div>
-            <div>
-              <span>8분 전</span>
-              <span>0개의 댓글</span>
-              <span>❤ 0</span>
-            </div>
-          </div>
+        {/* 여기는 맵으로 카드 뿌려주기 */}
+        <div
+          className="row row-cols-1 g-4"
+          style={{ maxWidth: '700px', border: '3px solid black', margin: '0' }}
+        >
+          {posts.map((data) => (
+            <PostCard key={data.postId} data={data} />
+          ))}
         </div>
       </div>
     </Layout>
@@ -71,8 +83,11 @@ const MyVelog = () => {
 
 export const Layout = styled.div`
   margin: 1% 5%;
-  border: 1px solid red;
-  height: 100vh;
+  /* border: 1px solid red; */
 `;
+
+// MyVelog.defaultProps = {
+//   posts: [],
+// };
 
 export default MyVelog;
