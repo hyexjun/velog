8fffd67072b8831adafb3515ec8d9faef32a9e6c
hyexjun <hyebyjun@gmail.com>
2022-08-25 13:33:19 +0900

    .

--- src/components/ComItem.jsx
+++ src/components/ComItem.jsx
@@ -5,7 +5,7 @@ const ComItem = () => {
     <div
       style={{
         maxWidth: '700px',
-        border: '1px solid green',
+        // border: '1px solid green',
         margin: '10px auto',
       }}
     >
@@ -18,7 +18,7 @@ const ComItem = () => {
       >
         <img
           style={{ borderRadius: '100%', width: '7%', height: '7%' }}
-          src="https://velog.velcdn.com/images/hyexjun/profile/108c8f1a-b604-4881-9906-00270be78272/image.jpg"
+          src="https://mblogthumb-phinf.pstatic.net/20160817_259/retspe_14714118890125sC2j_PNG/%C7%C7%C4%AB%C3%F2_%281%29.png?type=w800"
           alt="댓글 작성자 프사"
         />
         <div style={{ marginLeft: '10px' }}>
